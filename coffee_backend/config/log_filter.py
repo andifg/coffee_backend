dac62c82d6f229b3c6d88627d969da1eb4787c9c
anixel <45008199+andifg@users.noreply.github.com>
2024-10-17 21:09:44 +0200

    extend log filter to exclude metrics as well

--- coffee_backend/config/log_filter.py
+++ coffee_backend/config/log_filter.py
@@ -5,4 +5,7 @@ class HealthCheckFilter(logging.Filter):
     """Filter to exclude health check logs."""
 
     def filter(self, record: logging.LogRecord) -> bool:
-        return record.getMessage().find("/health") == -1
+        return (
+            record.getMessage().find("/health") == -1
+            and record.getMessage().find("/metrics") == -1
+        )
